2be3566e3c2bd2f39eee97fa759348872694fd01
Yongjun Rong <rongyj@hotmail.com>
2020-11-24 22:27:06 -0500

    fix the thubmprint for tls list

--- main.tf
+++ main.tf
@@ -108,6 +108,6 @@ resource "aws_iam_openid_connect_provider" "default" {
   url   = join("", aws_eks_cluster.default.*.identity.0.oidc.0.issuer)
 
   client_id_list = ["sts.amazonaws.com"]
-  thumbprint_list = concat([data.tls_certificate.cluster.certificates.0.sha1_fingerprint], var.oidc_thumbprint_list)
+  thumbprint_list = concat([data.tls_certificate.cluster.*.certificates.0.sha1_fingerprint], var.oidc_thumbprint_list)
   #url = aws_eks_cluster.default.identity.0.oidc.0.issuer
 }
